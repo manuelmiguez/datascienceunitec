f6bece65be829ab254de27d79aef2d05b98aa412
manuelmiguez <54189921+manuelmiguez@users.noreply.github.com>
2019-08-25 10:19:44 +1200

    Create corr.R

---
+++ Assignment Part A/corr.R
@@ -0,0 +1,19 @@
+corr <-function(directory="specdata/", threshold=0){
+    # variables for colllection
+    mydata <- vector()
+    
+    # create list of all .csv files in folder
+    file_list <- list.files(path=directory, pattern="*.csv") 
+    
+    # read each .csv file in file_list and process it
+    for (i in 1:length(file_list)){
+        thisdata <- read.csv(paste(directory, file_list[i], sep=''))[,2:3]
+        
+        if(nrow(thisdata)>threshold){
+            mydata <- c(mydata,cor(na.omit(thisdata))[1,2])
+        }# end if threshold
+    }#end for
+
+    #return
+    mydata
+}
