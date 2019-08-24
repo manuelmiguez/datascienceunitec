43da691661989590c942a8cd7f92225c7c0c322a
manuelmiguez <54189921+manuelmiguez@users.noreply.github.com>
2019-08-25 10:56:18 +1200

    Update pollutantmean.R

--- Assignment Part A/pollutantmean.R
+++ Assignment Part A/pollutantmean.R
@@ -1,6 +1,12 @@
 pollutantmean <- function(directory="specdata", pollutant="sulfate", id=1:332){
     mylist <- vector()
     
+    #check if last character is "/"
+    lastchar <- substr(directory, nchar(directory), nchar(directory))
+    if(lastchar=="/"){
+        directory <- substr(directory, 1, nchar(directory)-1)
+    }
+
     # run for each ID
     for(thisID in seq_along(id)){
         # define file path 
